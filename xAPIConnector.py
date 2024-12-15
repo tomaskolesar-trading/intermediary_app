80bed5c7f4a738942dc4476fa0a613a02b3901f8
tomaskolesar-trading <tomas.kolesar@email.cz>
2024-12-15 14:42:18 +0100

    Update xAPIConnector.py

--- xAPIConnector.py
+++ xAPIConnector.py
@@ -1,118 +1,178 @@
 import json
+import socket
 import logging
-from flask import Flask, request, jsonify
-from xAPIConnector import *
+import time
+import ssl
+from threading import Thread
 
-# Configure logging
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
-logger = logging.getLogger(__name__)
+# set to true on debug environment only
+DEBUG = True
 
-app = Flask(__name__)
+# default connection properties
+DEFAULT_XAPI_ADDRESS = 'xapi.xtb.com'
+DEFAULT_XAPI_PORT = 5124
+DEFUALT_XAPI_STREAMING_PORT = 5125
 
-# XTB Credentials
-XTB_USER_ID = "17190137"
-XTB_PASSWORD = "K193652744T"
+# wrapper name and version
+WRAPPER_NAME = 'python'
+WRAPPER_VERSION = '2.5.0'
 
-class XTBSession:
-    def __init__(self):
-        self.client = None
-        self.stream_client = None
-        self.stream_session_id = None
+# API inter-command timeout (in ms)
+API_SEND_TIMEOUT = 100
 
-    def authenticate(self):
-        try:
-            self.client = APIClient()
-            login_response = self.client.execute(
-                loginCommand(userId=XTB_USER_ID, password=XTB_PASSWORD, appName="Python Trading Bot")
-            )
-            logger.info(f"Login response: {login_response}")
-            
-            if login_response['status'] == True:
-                self.stream_session_id = login_response['streamSessionId']
-                return True
-            return False
-        except Exception as e:
-            logger.error(f"Authentication error: {e}")
-            return False
+# max connection tries
+API_MAX_CONN_TRIES = 3
 
-    def place_trade(self, symbol: str, action: str, volume: float):
-        if not self.client:
-            if not self.authenticate():
-                return {"error": "Failed to authenticate"}
+# logger properties
+logger = logging.getLogger("jsonSocket")
+FORMAT = '[%(asctime)-15s][%(funcName)s:%(lineno)d] %(message)s'
+logging.basicConfig(format=FORMAT)
 
-        cmd = TransactionSide.BUY if action.lower() == "buy" else TransactionSide.SELL
+if DEBUG:
+    logger.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.CRITICAL)
+
+class TransactionSide(object):
+    BUY = 0
+    SELL = 1
+    BUY_LIMIT = 2
+    SELL_LIMIT = 3
+    BUY_STOP = 4
+    SELL_STOP = 5
+    
+class TransactionType(object):
+    ORDER_OPEN = 0
+    ORDER_CLOSE = 2
+    ORDER_MODIFY = 3
+    ORDER_DELETE = 4
+
+class JsonSocket(object):
+    def __init__(self, address, port, encrypt = False):
+        self._ssl = encrypt 
+        if self._ssl != True:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket = ssl.wrap_socket(sock)
+        self.conn = self.socket
+        self._timeout = None
+        self._address = address
+        self._port = port
+        self._decoder = json.JSONDecoder()
+        self._receivedData = ''
+
+    def connect(self):
+        for i in range(API_MAX_CONN_TRIES):
+            try:
+                self.socket.connect( (self.address, self.port) )
+            except socket.error as msg:
+                logger.error("SockThread Error: %s" % msg)
+                time.sleep(0.25);
+                continue
+            logger.info("Socket connected")
+            return True
+        return False
+
+    def _sendObj(self, obj):
+        msg = json.dumps(obj)
+        self._waitingSend(msg)
+
+    def _waitingSend(self, msg):
+        if self.socket:
+            sent = 0
+            msg = msg.encode('utf-8')
+            while sent < len(msg):
+                sent += self.conn.send(msg[sent:])
+                logger.info('Sent: ' + str(msg))
+                time.sleep(API_SEND_TIMEOUT/1000)
+
+    def _read(self, bytesSize=4096):
+        if not self.socket:
+            raise RuntimeError("socket connection broken")
+        while True:
+            char = self.conn.recv(bytesSize).decode()
+            self._receivedData += char
+            try:
+                (resp, size) = self._decoder.raw_decode(self._receivedData)
+                if size == len(self._receivedData):
+                    self._receivedData = ''
+                    break
+                elif size < len(self._receivedData):
+                    self._receivedData = self._receivedData[size:].strip()
+                    break
+            except ValueError as e:
+                continue
+        logger.info('Received: ' + str(resp))
+        return resp
+
+    def _readObj(self):
+        msg = self._read()
+        return msg
+
+    def close(self):
+        logger.debug("Closing socket")
+        self._closeSocket()
+        if self.socket is not self.conn:
+            logger.debug("Closing connection socket")
+            self._closeConnection()
+
+    def _closeSocket(self):
+        self.socket.close()
+
+    def _closeConnection(self):
+        self.conn.close()
+
+    def _get_timeout(self):
+        return self._timeout
+
+    def _set_timeout(self, timeout):
+        self._timeout = timeout
+        self.socket.settimeout(timeout)
+
+    def _get_address(self):
+        return self._address
+
+    def _set_address(self, address):
+        pass
+
+    def _get_port(self):
+        return self._port
+
+    def _set_port(self, port):
+        pass
+
+    def _get_encrypt(self):
+        return self._ssl
+
+    def _set_encrypt(self, encrypt):
+        pass
+
+    timeout = property(_get_timeout, _set_timeout, doc='Get/set the socket timeout')
+    address = property(_get_address, _set_address, doc='read only property socket address')
+    port = property(_get_port, _set_port, doc='read only property socket port')
+    encrypt = property(_get_encrypt, _set_encrypt, doc='read only property socket port')
+
+class APIClient(JsonSocket):
+    def __init__(self, address=DEFAULT_XAPI_ADDRESS, port=DEFAULT_XAPI_PORT, encrypt=True):
+        super(APIClient, self).__init__(address, port, encrypt)
+        if(not self.connect()):
+            raise Exception("Cannot connect to " + address + ":" + str(port) + " after " + str(API_MAX_CONN_TRIES) + " retries")
+
+    def execute(self, dictionary):
+        self._sendObj(dictionary)
+        return self._readObj()    
+
+    def disconnect(self):
+        self.close()
         
-        transaction_info = {
-            "cmd": cmd,
-            "symbol": symbol,
-            "volume": volume,
-            "type": TransactionType.ORDER_OPEN,
-            "price": 0.0
-        }
+    def commandExecute(self,commandName, arguments=None):
+        return self.execute(baseCommand(commandName, arguments))
 
-        try:
-            response = self.client.execute({
-                "command": "tradeTransaction",
-                "arguments": {
-                    "tradeTransInfo": transaction_info
-                }
-            })
-            
-            logger.info(f"Trade response: {response}")
-            return response
-            
-        except Exception as e:
-            logger.error(f"Trade error: {e}")
-            return {"error": str(e)}
+def baseCommand(commandName, arguments=None):
+    if arguments==None:
+        arguments = dict()
+    return dict([('command', commandName), ('arguments', arguments)])
 
-# Initialize XTB session
-xtb_session = XTBSession()
-
-@app.route("/webhook", methods=["POST"])
-def webhook():
-    try:
-        data = request.json
-        logger.info(f"Received webhook: {data}")
-
-        if not all(key in data for key in ["action", "symbol", "volume"]):
-            return jsonify({"error": "Missing required fields"}), 400
-
-        action = data["action"].lower()
-        if action not in ["buy", "sell"]:
-            return jsonify({"error": "Invalid action"}), 400
-
-        result = xtb_session.place_trade(
-            symbol=data["symbol"],
-            action=action,
-            volume=float(data["volume"])
-        )
-        
-        if "error" in result:
-            return jsonify(result), 400
-        return jsonify(result)
-
-    except Exception as e:
-        logger.error(f"Webhook error: {e}")
-        return jsonify({"error": str(e)}), 500
-
-@app.route("/test-connection")
-def test_connection():
-    try:
-        result = xtb_session.authenticate()
-        return jsonify({
-            "connected": result,
-            "session_id": xtb_session.stream_session_id
-        })
-    except Exception as e:
-        return jsonify({"error": str(e)})
-
-@app.route("/")
-def index():
-    return "XTB TradingView Webhook Listener is running!"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+def loginCommand(userId, password, appName=''):
+    return baseCommand('login', dict(userId=userId, password=password, appName=appName))
